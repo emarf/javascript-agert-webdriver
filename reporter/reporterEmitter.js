b27563dd520078ab8f44e6da2b78f78ad072f698
emarf <rivashkevich@solvd.com>
2022-02-01 12:33:06 +0300

    fix: change artifacts implementation

--- reporter/reporterEmitter.js
+++ reporter/reporterEmitter.js
@@ -2,29 +2,34 @@ export const reporterEmitter = {
   setMaintainer: (maintainer) => {
     process.emit('SET_MAINTAINER', maintainer)
   },
-  
-  setRunArtifactAttachments: (runArtifacts) => {
-    process.emit('SET_RUN_ARTIFACTS', runArtifacts)
-  },
-  setTestArtifactAttachments: (testArtifacts) => {
-    process.emit('SET_TEST_ARTIFACTS', testArtifacts)
-  },
   setRunLabels: (labels) => {
     process.emit('SET_RUN_LABELS', labels)
   },
   setTestLabels: (labels) => {
     process.emit('SET_TEST_LABELS', labels)
   },
-  setTestLogs: (logs) => {
-    process.emit('SET_TEST_LOGS', logs)
-  },
-  revertTestRegistration: () => {
-    process.emit('REVERT_TEST_REGISTRATION',)
-  },
   setRunTcmOptions: (options) => {
     process.emit('SET_RUN_TCM_OPTIONS', options);
   },
   setTestTcmOptions: (options) => {
     process.emit('SET_TEST_TCM_OPTIONS', options);
+  },
+  attachToTestRun: (attachments) => {
+    process.emit('ATTACH_TO_TEST_RUN', attachments);
+  },
+  attachReferenceToTestRun: (references) => {
+    process.emit('ATTACH_REF_TO_TEST_RUN', references);
+  },
+  attachToTest: (attachments) => {
+    process.emit('ATTACH_TO_TEST', attachments);
+  },
+  attachReferenceToTest: (references) => {
+    process.emit('ATTACH_REF_TO_TEST', references);
+  },
+  setTestLogs: (logs) => {
+    process.emit('SET_TEST_LOGS', logs)
+  },
+  revertTestRegistration: () => {
+    process.emit('REVERT_TEST_REGISTRATION',)
   },
 };
